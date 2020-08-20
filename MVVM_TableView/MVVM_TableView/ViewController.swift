92ee2df6ceac40a227ce4b13578d54ac94cfc8c5
VICTOR HUGO BENITEZ BOSQUES <vbenitez@bancoazteca.com>
2020-08-20 17:17:15 -0500

    Create project to  build a simple application using the MVVM pattern

---
+++ MVVM_TableView/MVVM_TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_TableView
+//
+//  Created by Victor Hugo Benitez Bosques on 8/20/20.
+//  Copyright © 2020 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
